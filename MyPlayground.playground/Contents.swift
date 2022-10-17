c1ad6a5ba1a39091e8265e72d59b18297dfd011a
ThoSamSet <puansuanzun@gmail.com>
2022-10-17 15:52:14 +0900

    khai báo biến số và hằng số

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,6 +6,22 @@ greeting = "Hello, playground"
 var name:String = "Phan Xuân Dũng"
 var age = 23
 
+//Khai báo hằng số
+let dayLaHangSo : String
+dayLaHangSo = "VIỆT NAM"
+
+//Tạo bí danh
+typealias so = Int      //Định nghĩa 'so' là kiểu 'Int'
+var testSo:so = 123
+
 //In kết quả
-print(name)
-print(age)
+//print(name)
+//print(age)
+//
+//print(testSo)
+//print(dayLaHangSo)
+
+//Nội suy giá trị của biến trong chuôi in
+print("Họ và tên: \(name), tuổi: \(age), Quốc Tịch: \(dayLaHangSo)")
+
+
